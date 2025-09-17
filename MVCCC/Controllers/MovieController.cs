e3c304eaa7b85ff4b4c88e15661effdba5353f47
ahmedWalid21 <420230110@fa-hists.edu.eg>
2025-09-17 18:28:56 +0300

    V05

--- MVCCC/Controllers/MovieController.cs
+++ MVCCC/Controllers/MovieController.cs
@@ -5,13 +5,73 @@ namespace MVCCC.Controllers
     public class MovieController:Controller
     {
         //Action: public non-static method in a controller class    
-        public string GetMovie(int id)
-        {
-            return $"Get Movie With Id: {id}";
-        }
+        //Action: has a spacific return type
+        //ActionResult
+        //public string GetMovie(int id)
+        //{
+        //    return $"Get Movie With Id: {id}";
+        //}
         public string AddMovie(int id)
         {
             return $"Add Movie With Id: {id}";
         }
+
+        //public ContentResult GetMovie(int id)
+        //{
+        //    var contentResult = new ContentResult()
+        //    {
+        //        Content = $"Get Movie With Id: {id}",
+        //        ContentType = "text/html",
+        //        StatusCode = 200
+
+        //    };
+        //    return contentResult;
+        //}
+        //public ContentResult GetMovie(int id)
+        //{
+        //    var contentResult = new ContentResult()
+        //    {
+        //        Content = $"Get Movie With Id: {id}",
+        //        ContentType = "object/pdf",
+        //        StatusCode = 200
+
+        //    };
+        //    return contentResult;
+        //}
+        //public RedirectResult GetMovie(int id)
+        //{
+        //   var redirectResult = new RedirectResult("https://www.google.com");
+        //    return redirectResult;
+        //}
+        //public RedirectToActionResult GetMovie(int id)
+        //{
+        //    var redirectResult = new RedirectToActionResult("AddMovie", "Movie", new { id = 133 });
+        //    return redirectResult;
+        //}
+        [ActionName("GetMovie")]
+        public IActionResult GetMovieByCategory(int id)
+        {
+            if (id == 1)
+            {
+                //return new ContentResult(){Content = $"Get Movie With Id: {id}",ContentType = "text/html"  };
+                return Content($"Get Movie With Id: {id}", "text/html");
+            }
+            else if(id==2)
+            {
+                return new ContentResult()
+                {
+                    Content = $"Get Movie With Id: {id}",
+                    ContentType = "object/pdf"
+                };
+            }
+            else
+            {
+                return new RedirectResult("https://www.google.com");    
+            }
+        }
+
+
+
+
     }
 }
